e27d833e92de006197193eaccc4c4211e52007ad
goalandpriority-ui <goalandpriority@gmail.com>
2026-03-21 19:18:17 +0530

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,10 +2,16 @@ import { createClient } from "@supabase/supabase-js"
 
 export const getSupabase = () => {
 
-  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-  const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY
+  const supabaseUrl =
+    process.env.SUPABASE_URL ||
+    process.env.NEXT_PUBLIC_SUPABASE_URL
+
+  const supabaseKey =
+    process.env.SUPABASE_SERVICE_ROLE_KEY ||
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
   if (!supabaseUrl) {
+    console.error("ENV:", process.env)
     throw new Error("Missing SUPABASE URL")
   }
 
