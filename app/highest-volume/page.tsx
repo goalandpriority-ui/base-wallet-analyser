4f9a1805a2ccd210c3327116e7fe1d673216e681
goalandpriority-ui <goalandpriority@gmail.com>
2026-03-23 23:51:46 +0530

    Update page.tsx

--- app/highest-volume/page.tsx
+++ app/highest-volume/page.tsx
@@ -19,8 +19,17 @@ useEffect(()=>{
 fetch(`/api/highest-volume?page=${page}&wallet=${wallet||""}`)
 .then(res=>res.json())
 .then(res=>{
-setData(res.data || [])
+
+/* FIX FIELD NAMES */
+const mapped = (res.data || []).map((w:any)=>({
+...w,
+volume: w.tradingVolumeUSD || 0,
+swaps: w.swapCount || 0
+}))
+
+setData(mapped)
 setRank(res.yourRank)
+
 })
 
 },[page])
