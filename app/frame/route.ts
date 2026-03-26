787569b32af9283612ce815aaa145e95bd8ad6cf
goalandpriority-ui <goalandpriority@gmail.com>
2026-03-26 13:17:32 +0530

    Update route.ts

--- app/frame/route.ts
+++ app/frame/route.ts
@@ -1,31 +1,26 @@
 import { NextResponse } from "next/server"
 
 export async function GET() {
-
-return new NextResponse(`
-<!DOCTYPE html>
+  return new NextResponse(`<!DOCTYPE html>
 <html>
 <head>
 
-<meta property="fc:frame" content="vNext" />
-<meta property="fc:frame:image" content="https://base-wallet-analyser.vercel.app/splash.png" />
-
-<meta property="fc:frame:button:1" content="Open App" />
-<meta property="fc:frame:button:1:action" content="link" />
-<meta property="fc:frame:button:1:target" content="https://base-wallet-analyser.vercel.app" />
-
 <meta property="og:title" content="Base Wallet Analyser" />
 <meta property="og:image" content="https://base-wallet-analyser.vercel.app/splash.png" />
+
+<meta name="fc:frame" content="vNext" />
+<meta name="fc:frame:image" content="https://base-wallet-analyser.vercel.app/splash.png" />
+<meta name="fc:frame:button:1" content="Open App" />
+<meta name="fc:frame:button:1:action" content="link" />
+<meta name="fc:frame:button:1:target" content="https://base-wallet-analyser.vercel.app" />
 
 </head>
 <body>
 Base Wallet Analyser
 </body>
-</html>
-`,{
-headers:{
-"Content-Type":"text/html"
-}
-})
-
+</html>`, {
+    headers: {
+      "Content-Type": "text/html"
+    }
+  })
 }
